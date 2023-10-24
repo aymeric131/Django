33c86bdc3d2ca16e2d3ec153f786ec5fb4a2a380
aymeric131 <aymeric.ludo@gmail.com>
2023-10-23 20:17:53 -0400

    Admin&Models

--- merchex/listings/admin.py
+++ merchex/listings/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from listings.models import *
+
+class BandAdmin(admin.ModelAdmin):  # nous insérons ces deux lignes..
+    list_display = ('name', 'year_formed', 'genre')
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('title','sold','year','type')
+
+admin.site.register(Band,BandAdmin)
+admin.site.register(Listing,ListingAdmin)
--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -1,10 +1,33 @@
 from django.db import models
+from django.core.validators import *
+
 
 class Band(models.Model):
-    name = models.fields.CharField(max_length=100)
+    class Genre(models.TextChoices):
+        HIP_HOP = 'HH'
+        SYNTH_POP = 'SP'
+        ALTERNATIVE_ROCK = 'AR'
 
+    name = models.fields.CharField(max_length=100)
+    genre = models.fields.CharField(choices=Genre.choices, max_length=5)
+    biography = models.fields.CharField(max_length=100)
+    year_formed = models.fields.IntegerField(validators=[MinValueValidator(1900),MaxValueValidator(2023)])
+    active = models.fields.BooleanField(default=True)
+    official_page = models.fields.URLField(null=True, blank=True)
+    def __str__(self):
+        return f'{self.name}'
 
 class Listing(models.Model):
+    class Type(models.TextChoices):
+        RECORDS = 'RCRDS'
+        CLOTHING = 'CLTHG'
+        POSTERS = 'PSTRS'
+        MISCELLANEOUS = 'MSCLNS'
     title = models.fields.CharField(max_length=100)
-
+    description = models.fields.CharField(max_length=100)
+    sold = models.fields.BooleanField(default=False)
+    year = models.fields.IntegerField(validators=[MinValueValidator(0),MaxValueValidator(2023)],blank=True)
+    type = models.fields.CharField(choices=Type.choices, max_length=10)
+    def __str__(self):
+        return f'{self.title}'
     
